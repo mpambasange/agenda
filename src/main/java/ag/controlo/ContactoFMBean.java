f3374ff9860cb88dd3a7c5b6fd31b3dceca422a8
manuelpambasagejorge <manuelpambasagejorge@air-de-manuel.lan>
2020-08-27 15:41:47 +0100

    Envio do projecto ao repositório

---
+++ src/main/java/ag/controlo/ContactoFMBean.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ag.controlo;
+
+import ag.modelo.Contacto;
+import javax.annotation.PostConstruct;
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+
+/**
+ *
+ * @author manuelpambasagejorge
+ */
+@Named(value = "contactoFMBean")
+@RequestScoped
+public class ContactoFMBean {
+    
+    Contacto contacto = new Contacto ();
+
+    @PostConstruct
+    public void init () {
+    }
+
+    public Contacto getContacto() {
+        return contacto;
+    }
+
+    public void setContacto(Contacto contacto) {
+        this.contacto = contacto;
+    }
+    
+    
+    public String guardar () {
+        return "adicionado";
+    }
+    
+   
+    
+}
---
+++ src/main/java/ag/modelo/Contacto.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ag.modelo;
+
+/**
+ *
+ * @author manuelpambasagejorge
+ */
+public class Contacto {
+    private String nome;
+    private String sobrenome;
+    private String email;
+    private String telefone;
+
+    public Contacto() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+    
+    
+    
+    
+}
